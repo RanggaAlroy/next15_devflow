48f781963804d96d9e296a2841ddb9d298ef40bb
Rangga Alroy <rangga.alroy@gmail.com>
2024-12-09 10:39:48 +0700

    setup Auth, with next-auth, github and google

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,26 @@
-const Home = () => {
-  <>
-    <h1 className="h1-bold">Hello this is Stater Next js development</h1>
-  </>;
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constant/routes";
+
+// Server Action for handling sign-out
+export async function handleSignOut() {
+  "use server";
+  await signOut({ redirectTo: ROUTES.SIGN_IN });
+}
+
+const Home = async () => {
+  const session = await auth();
+  console.log(session);
+
+  return (
+    <>
+      <h1 className="h2-bold">Hello, this is Starter Next.js development</h1>
+
+      <form action={handleSignOut} className="mt-[100px] px-10">
+        <Button type="submit">Log out</Button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
