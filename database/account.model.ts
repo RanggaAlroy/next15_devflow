a98f3c4e7e26998e4a911a301b9f21e0e3faba2c
RanggaAlroy <114912822+RanggaAlroy@users.noreply.github.com>
2024-12-31 21:05:36 +0700

    account schema fix name

--- database/account.model.ts
+++ database/account.model.ts
@@ -23,6 +23,6 @@ const AccountSchema = new Schema<IAccount>(
     }
 )
 
-const Account = models?.account || model<IAccount>('account', AccountSchema);
+const Account = models?.Account || model<IAccount>('Account', AccountSchema);
 
 export default Account
