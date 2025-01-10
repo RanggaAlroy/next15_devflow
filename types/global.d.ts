dd1e44b2af7c64fb7cafd6e8ab12e1d9ba715bf2
Rangga Alroy <rangga.alroy@gmail.com>
2025-01-10 15:22:05 +0700

    implement , create and read user Route

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 
 interface Tags {
     _id: string;
@@ -19,3 +20,20 @@ interface Question {
     createdAt: Date;
     author: Author;
 }
+
+
+type ActionResponse<T = null> = {
+    success: boolean;
+    data?: T;
+    error?: {
+      message: string;
+      details?: Record<string, string[]>;
+    };
+    status?: number;
+  };
+
+type SuccessResponse<T = null> = ActionResponse<T> & { success: true };
+type ErrorResponse = ActionResponse<undefined> & { success: false };
+
+type APIErrorResponse = NextResponse<ErrorResponse>;
+type APIResponse<T = null> = NextResponse<SuccessResponse<T> | ErrorResponse>;
